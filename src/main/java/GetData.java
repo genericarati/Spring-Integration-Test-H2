9148524da9644c5033a89da685e52b2647b54c74
four-words-all-uppercase <gotdgoe@ford.com>
2018-03-11 17:30:45 -0400

    Can call h2 stored proc

---
+++ src/main/java/GetData.java
@@ -0,0 +1,34 @@
+package hello;
+
+import org.springframework.stereotype.Service;
+
+import java.sql.*;
+
+@Service
+public class GetData {
+
+    public static  String getTableContent() throws Exception {
+        String resultValue=null;
+
+        Connection conn = DriverManager.getConnection( "jdbc:h2:file:./test","sa", "");
+        java.sql.ResultSet rs = conn.createStatement().executeQuery(
+                " SELECT * FROM TEST");
+        while(rs.next())
+        {
+            resultValue=rs.getString(3);
+        }
+        return resultValue;
+    }
+
+    public void createStoredProc() throws SQLException {
+        Connection conn = DriverManager.getConnection( "jdbc:h2:file:./test","sa", "");
+        Statement st = conn.createStatement();
+        st.execute("DROP ALIAS IF EXISTS getTableContent");
+        st.execute("CREATE ALIAS getTableContent FOR \"hello.GetData.getTableContent\"");
+        ResultSet resultSet = st.executeQuery("CALL getTableContent()");
+        while(resultSet.next()) {
+            System.out.println("Version: " + resultSet.getString(1));
+        }
+    }
+
+}
--- src/main/java/HelloController.java
+++ src/main/java/HelloController.java
@@ -1,13 +1,27 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import hello.GetData;
+
+import java.sql.SQLException;
 
 @RestController
 public class HelloController {
 
+
+    GetData getData;
+
+    @Autowired
+    public HelloController(GetData getData) {
+        this.getData = getData;
+    }
+
+
     @RequestMapping("/")
-    public String index() {
+    public String index() throws SQLException {
+        getData.createStoredProc();
         return "Greetings from Spring Boot!";
     }
 
